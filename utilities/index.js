d86fa73829f9639dd0fd93a28188e1c59ceed74a
oseimacdonald <oseimacdonald@gmail.com>
2025-09-18 13:03:08 +0000

    a

--- utilities/index.js
+++ utilities/index.js
@@ -25,6 +25,35 @@ Util.getNav = async function (req, res, next) {
 }
 
 
+/* ******************************************************
+*  Build the classification view html
+* ****************************************************** */
+Util.buildClassificationGrid = async function(data){
+    let grid
+    if(data.length > 0){
+        grid = 'ul id="inv-display">'
+        data.forEach(vehicle => {
+            grid += 'li'
+            grid += '<a href="../../inv/detail/'+ vehicle.inv_id
+            +'" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model
+            + 'details"><img src="' + vehicle.inv_thumbnail
+            +'" alt="image of '+ vehicle.inv_make + ' ' + vehicle.inv_model
+            +' on CSE Motors" /></a>'
+            grid += '<div class="namePrice">'
+            grid += '<hr />'
+            grid += '<h2>'
+            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View '
+            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">'
+            + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+            grid += '</div>'
+            grid += '</li>'
+        })
+        grid += '</ul>'
+    } else {
+        grid += '<p class="notice">sorry, no matching vehicles could be found.</p>'
+    }
+    return grid
+}
 
 
 module.exports = Util
