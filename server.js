0d4fcfaa20ba764eb9c0ee83507cfa802fbfa3c9
oseimacdonald <oseimacdonald@gmail.com>
2025-09-18 15:10:10 +0000

    c

--- server.js
+++ server.js
@@ -36,13 +36,12 @@ app.use("/inv", inventoryRoute)
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500;
-const host = process.env.HOST || "localhost";
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
-  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`)
 })
